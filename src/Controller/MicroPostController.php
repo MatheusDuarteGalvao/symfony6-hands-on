44b5ca7caf9c757ea02f5841a8fa31a661d11a2c
Matheus Duarte <matheusduartegalvao@gmail.com>
2024-01-22 20:39:03 -0300

    feat: adding insert data to database

--- src/Controller/MicroPostController.php
+++ src/Controller/MicroPostController.php
@@ -4,8 +4,10 @@ namespace App\Controller;
 
 use App\Entity\MicroPost;
 use App\Repository\MicroPostRepository;
+use DateTime;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -28,7 +30,7 @@ class MicroPostController extends AbstractController
     }
 
     #[Route('/micro-post/add', name: 'app_micro_post_add', priority: 2)]
-    public function add(): Response
+    public function add(Request $request, MicroPostRepository $posts): Response
     {
         $microPost = new MicroPost();
         $form = $this->createFormBuilder($microPost)
@@ -36,6 +38,14 @@ class MicroPostController extends AbstractController
             ->add('text')
             ->add('submit', SubmitType::class, ['label' => 'Save'])
             ->getForm();
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $post = $form->getData();
+            $post->setCreated(new DateTime());
+            $posts->add($post, true);
+        }
 
         return $this->render(
             'micro_post/add.html.twig',
