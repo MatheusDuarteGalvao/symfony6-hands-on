22b7ecdf83385b3b0ad2c7e815904897175c99b8
Matheus Duarte <matheusduartegalvao@gmail.com>
2024-02-28 21:19:36 -0300

    feat: adding comment section to the post show template and improving breadcrumbs

--- src/Controller/MicroPostController.php
+++ src/Controller/MicroPostController.php
@@ -77,7 +77,8 @@ class MicroPostController extends AbstractController
         return $this->render(
             'micro_post/edit.html.twig',
             [
-                'form' => $form
+                'form' => $form,
+                'post' => $post
             ]
         );
     }
