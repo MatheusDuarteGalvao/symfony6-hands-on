1355f3a512290bd77fb4f36c40a5323aefab279f
Matheus Duarte <matheusduartegalvao@gmail.com>
2024-05-21 19:32:56 -0300

    feat: adding profile settings to the register page and save profile function

--- src/Controller/SettingsProfileController.php
+++ src/Controller/SettingsProfileController.php
@@ -5,16 +5,21 @@ namespace App\Controller;
 use App\Entity\User;
 use App\Entity\UserProfile;
 use App\Form\UserProfileType;
+use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 class SettingsProfileController extends AbstractController
 {
     #[Route('/settings/profile', name: 'app_settings_profile')]
-    public function profile(Request $request): Response
-    {
+    #[IsGranted('IS_AUTHENTICATED_FULLY')]
+    public function profile(
+        Request $request,
+        UserRepository $users
+    ): Response {
         /** @var User $user */
         $user = $this->getUser();
         $userProfile = $user->getUserProfile() ?? new UserProfile();
@@ -25,7 +30,17 @@ class SettingsProfileController extends AbstractController
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
-            $userProfile = $form->getData();         
+            $userProfile = $form->getData();
+            $user->setUserProfile($userProfile);
+            $users->save($user, true);
+            $this->addFlash(
+                'success',
+                'Your user profile settings were saved'
+            );
+
+            return $this->redirectToRoute(
+                'app_settings_profile'
+            );
         }
 
         return $this->render(
